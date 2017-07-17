1f0bb7eccf5ca494676e5a92f8849a710b56d162
Hugo Daniel <hugodaniel.icam@gmail.com>
2017-07-17 18:00:30 +0200

    a

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,5 +1,5 @@
 class DosesController < ApplicationController
-  before_action :set_cocktail
+  before_action :set_cocktail, except: [:destroy]
 
   def new
     @dose = Dose.new
@@ -15,7 +15,6 @@ class DosesController < ApplicationController
   end
 
   def destroy
-    @dose = @cocktail.doses.find(params[:id])
     @dose.destroy
 
     redirect_to cocktail_path(@cocktail)
